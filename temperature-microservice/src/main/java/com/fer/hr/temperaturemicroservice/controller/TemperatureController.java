3c6ebc8041d596738854384efd918fa105322b32
fmaznik <fran.maznik@gmail.com>
2025-01-13 04:24:54 +0100

    Add logger to temperature controller

--- temperature-microservice/src/main/java/com/fer/hr/temperaturemicroservice/controller/TemperatureController.java
+++ temperature-microservice/src/main/java/com/fer/hr/temperaturemicroservice/controller/TemperatureController.java
@@ -1,5 +1,7 @@
 package com.fer.hr.temperaturemicroservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fer.hr.temperaturemicroservice.model.Temperature;
 import com.fer.hr.temperaturemicroservice.service.TemperatureService;
 import jakarta.persistence.GeneratedValue;
@@ -9,20 +11,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/temperature")
 public class TemperatureController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TemperatureController.class); // Logger initialization
+
     private final TemperatureService temperatureService;
 
-    public TemperatureController(TemperatureService temperatureService) {
+
+
+    public TemperatureController(TemperatureService temperatureService, ObjectMapper objectMapper) {
         this.temperatureService = temperatureService;
     }
 
     @GetMapping("/current")
-    public ResponseEntity<Temperature> getTemperature() {
-        var temperatureOpt = temperatureService.getCurrentTemperature();
+    public ResponseEntity<Temperature> getTemperature() throws JsonProcessingException {
+        logger.info("Fetching current temperature data");
 
-        return temperatureOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+        var temperatureOpt = temperatureService.getCurrentTemperature();
+        if (temperatureOpt.isPresent()) {
+
+            logger.info("Current temperature " +temperatureOpt.get()+ " retrieved successfully");
+            return ResponseEntity.ok(temperatureOpt.get());
+        } else {
+            logger.warn("Current temperature not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
     }
 }
